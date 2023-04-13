ca0fbd6d780ef2d9e8008e3c758679a513b5d59f
Ricardo <ricardo.rdzalt@gmail.com>
2023-04-12 22:02:48 -0700

    .

--- main.py
+++ main.py
@@ -1,48 +1,72 @@
+import cv2
 import gi
 gi.require_version('Gst', '1.0')
-from gi.repository import Gst, GObject
-import cv2
-import numpy as np
+from gi.repository import Gst, GObject, GLib
 
-GObject.threads_init()
+# Inicializamos GStreamer y GObject
 Gst.init(None)
+GObject.threads_init()
 
-def on_new_sample(appsink):
-    sample = appsink.emit("pull-sample")
-    buffer = sample.get_buffer()
-    result, info = buffer.map(Gst.MapFlags.READ)
-    if result:
-        data = info.data
-        img = cv2.imdecode(np.frombuffer(data, np.uint8), -1)
-        cv2.imshow("Video", img)
-        cv2.waitKey(1)
-        buffer.unmap(info)
+# Registramos los elementos de cámara de Raspberry Pi
+Gst.ElementFactory.register(Gst.ElementFactory.find("libcamerasrc"), 1)
+Gst.ElementFactory.register(Gst.ElementFactory.find("RaspiCamSrc"), 1)
 
+# Creamos el pipeline de GStreamer
 pipeline = Gst.Pipeline()
 
-# Agregamos el elemento libcamerasrc
-camerasrc = Gst.ElementFactory.make("libcamerasrc")
+# Creamos los elementos necesarios
+camerasrc = Gst.ElementFactory.make('libcamerasrc', 'camera-source')
+camerasrc.set_property('sensor-id', 0)
+videoscale = Gst.ElementFactory.make('videoscale', 'video-scaler')
+capsfilter = Gst.ElementFactory.make('capsfilter', 'capsfilter')
+caps = Gst.Caps.from_string('video/x-raw,width=640,height=480')
+capsfilter.set_property('caps', caps)
+videoconvert = Gst.ElementFactory.make('videoconvert', 'video-converter')
+appsink = Gst.ElementFactory.make('appsink', 'app-sink')
+appsink.set_property('emit-signals', True)
+appsink.set_property('sync', False)
 
-# Agregamos el elemento appsink para recibir los frames del video
-appsink = Gst.ElementFactory.make("appsink")
-appsink.set_property("emit-signals", True)
-appsink.set_property("sync", False)
-appsink.connect("new-sample", on_new_sample)
-
-# Agregamos los elementos al pipeline
+# Añadimos los elementos al pipeline
 pipeline.add(camerasrc)
+pipeline.add(videoscale)
+pipeline.add(capsfilter)
+pipeline.add(videoconvert)
 pipeline.add(appsink)
 
 # Conectamos los elementos
-camerasrc.link(appsink)
+camerasrc.link(videoscale)
+videoscale.link(capsfilter)
+capsfilter.link(videoconvert)
+videoconvert.link(appsink)
 
-# Iniciamos la reproducción
+# Creamos la función para manejar los nuevos frames del AppSink
+def on_new_sample(appsink):
+    sample = appsink.emit('pull-sample')
+    buffer = sample.get_buffer()
+    caps = sample.get_caps()
+    width = caps.get_structure(0).get_value('width')
+    height = caps.get_structure(0).get_value('height')
+    nparr = buffer.extract_dup(0, buffer.get_size())
+    frame = np.frombuffer(nparr, np.uint8).reshape((height, width, 3))
+    return frame
+
+# Conectamos la señal "new-sample" del AppSink a la función "on_new_sample"
+appsink.connect('new-sample', on_new_sample)
+
+# Iniciamos el pipeline
 pipeline.set_state(Gst.State.PLAYING)
 
-# Esperamos a que se cierre el pipeline
-bus = pipeline.get_bus()
-msg = bus.timed_pop_filtered(Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
+# Creamos la ventana de OpenCV
+cv2.namedWindow('Video', cv2.WINDOW_NORMAL)
 
-# Paramos la reproducción y liberamos los recursos
-pipeline.set_state(Gst.State.NULL)
-cv2.destroyAllWindows()
+# Bucle principal
+while True:
+    # Obtenemos un frame del AppSink
+    frame = on_new_sample(appsink)
+
+    # Mostramos el frame en la ventana de OpenCV
+    cv2.imshow('Video', frame)
+    # Si se presiona la tecla 'q', salimos del bucle
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
